b0d977423a905f663c4c8a0b42f2b6561836f6b3
Michael Handley <handley@mit.edu>
2014-07-17 02:20:01 -0400

    re

--- nodeServer.js
+++ nodeServer.js
@@ -212,7 +212,7 @@ require('./routes.js')(app); //Load webpage routes
 app.post("/tryJson", function(req, res) {
     console.log("ajax request received!!");
     console.log(req.data);
-    console.log(JSON.parse(data))
+    console.log(JSON.parse(req.data))
     console.log(req.body);
     res.writeHead(200, {'Content-Type': 'text/plain'});
     
